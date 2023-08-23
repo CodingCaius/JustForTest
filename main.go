818ebf80ed0dbec826f90307bca5eec910a42085
caius <3085481534@qq.com>
2023-08-23 20:45:21 +0800

    添加个结构体

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+
+type DocContentResponse struct {
+	Content   string `json:"content"`
+	IsSee     bool   `json:"isSee"`     // 是否试看
+	IsPwd     bool   `json:"isPwd"`     // 是否需要密码
+	IsColl    bool   `json:"isColl"`    // 用户是否收藏
+	LookCount int    `json:"lookCount"` // 浏览量
+	DiggCount int    `json:"diggCount"` // 点赞量
+	CollCount int    `json:"collCount"` // 收藏量
+}
+
 func main() {
 	fmt.Println("铃木爱理嫁给我")
 	fmt.Println("我的最爱")
